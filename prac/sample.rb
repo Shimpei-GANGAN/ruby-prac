713592a59ca613fb425af4eb23dc05451e697283
gangan <gangan.education@gmail.com>
2022-07-23 22:46:26 +0900

    prac: ~Q26

--- prac/sample.rb
+++ prac/sample.rb
@@ -19,30 +19,49 @@
 # printf("finished:%d\n", sum)
 
 # Q23
-tango = ["knowledge", "information", "system", "library", "metadata"]
+# tango = ["knowledge", "information", "system", "library", "metadata"]
 
-while true
-  puts("単語を入力してください")
-  puts("終わる時は . (ピリオド)を入力してください")
-  a = gets.chomp
-  if a == "."
-    break
-  end
+# while true
+#   puts("単語を入力してください")
+#   puts("終わる時は . (ピリオド)を入力してください")
+#   a = gets.chomp
+#   if a == "."
+#     break
+#   end
 
-  j = 0
-  while j < tango.size
-    if a == tango[j]
-      printf("%sは辞書に登録されています\n", a)
-      break
-    end
-  j += 1
-  end
-  if j == tango.size
-    printf("%sを登録します\n", a)
-    tango.push(a)
-  end
-end
+#   j = 0
+#   while j < tango.size
+#     if a == tango[j]
+#       printf("%sは辞書に登録されています\n", a)
+#       break
+#     end
+#   j += 1
+#   end
+#   if j == tango.size
+#     printf("%sを登録します\n", a)
+#     tango.push(a)
+#   end
+# end
 
-tango.each{|value|
-  puts(value)
-}
+# tango.each{|value|
+#   puts(value)
+# }
+
+# Q24
+str = "Hi\n"
+print(str, "!\n")
+
+str1 = str.chomp
+str2 = str.chomp.chomp
+str3 = str.chop
+str4 = str.chop.chop
+
+print(str1, "!\n")
+print(str2, "!\n")
+print(str3, "!\n")
+print(str4, "!\n")
+
+# Q25
+puts('名前を入力してください.')
+name = gets.chomp
+printf("%sさん, こんにちは\n", name)
