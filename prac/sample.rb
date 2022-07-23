71d5f6be051934702e0868f645ba51a057b21159
gangan <gangan.education@gmail.com>
2022-07-23 22:32:41 +0900

    prac:: ~Q23

--- prac/sample.rb
+++ prac/sample.rb
@@ -9,11 +9,40 @@
 # end
 
 # Q21
-i = 0
-sum = 0
-while i <= 10
-  sum += i
-  puts(sum)
-  i += 1
+# i = 0
+# sum = 0
+# while i <= 10
+#   sum += i
+#   puts(sum)
+#   i += 1
+# end
+# printf("finished:%d\n", sum)
+
+# Q23
+tango = ["knowledge", "information", "system", "library", "metadata"]
+
+while true
+  puts("単語を入力してください")
+  puts("終わる時は . (ピリオド)を入力してください")
+  a = gets.chomp
+  if a == "."
+    break
+  end
+
+  j = 0
+  while j < tango.size
+    if a == tango[j]
+      printf("%sは辞書に登録されています\n", a)
+      break
+    end
+  j += 1
+  end
+  if j == tango.size
+    printf("%sを登録します\n", a)
+    tango.push(a)
+  end
 end
-printf("finished:%d\n", sum)
+
+tango.each{|value|
+  puts(value)
+}
