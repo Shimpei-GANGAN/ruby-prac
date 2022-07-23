8b8eaf5efb4eef965334a4bb6a6cf8ffde10290e
gangan <gangan.education@gmail.com>
2022-07-23 22:31:44 +0900

    prac: ~Q21

--- prac/sample.rb
+++ prac/sample.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 # Q15
-x = 8
-if x >= 1 && x <= 10
-  printf("%dは1から10の間にあります\n", x)
-else
-  printf("%dは1から10の間にありません\n", x)
+# x = 8
+# if x >= 1 && x <= 10
+#   printf("%dは1から10の間にあります\n", x)
+# else
+#   printf("%dは1から10の間にありません\n", x)
+# end
+
+# Q21
+i = 0
+sum = 0
+while i <= 10
+  sum += i
+  puts(sum)
+  i += 1
 end
+printf("finished:%d\n", sum)
