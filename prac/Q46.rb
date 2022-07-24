d766866a8c861b7da2764edc2b0d252796d31f87
gangan <gangan.education@gmail.com>
2022-07-24 17:55:54 +0900

    D < 0

--- prac/Q46.rb
+++ prac/Q46.rb
@@ -8,6 +8,7 @@ end
 # NOTE: 2次方程式
 def quadratic(a, b, c)
   d = discriminant(a, b, c)
+  a2 = a * 2
 
   if d.positive?
     x1 = ((-b + Math.sqrt(d)) / (a * 2)).round(2)
@@ -16,17 +17,25 @@ def quadratic(a, b, c)
     [x1, x2]
   elsif d.zero?
     x1 = ((-b / (a * 2))).round(2)
+
     [x1]
   else
-    '実数解なし'
+    # NOTE: Math.sqrtの返り値がunsignedのため, 結果に-1をかけている
+    x1 = Complex(Rational(-b, a2), Rational(Math.sqrt(discriminant(a, b, c) * -1), a2))
+    x2 = Complex(Rational(-b, a2), Rational(- Math.sqrt(discriminant(a, b, c) * -1), a2))
+
+    [x1, x2]
   end
 end
 
 # ----------------------------------
-puts quadratic(1, 1, -6).to_s
-puts quadratic(6, 7, -3).to_s
-puts quadratic(1, -5, 4).to_s
-puts quadratic(1, 8, 16).to_s
-puts quadratic(2, 4, 1).to_s
-puts quadratic(9, 6, 1).to_s
-puts quadratic(3, 3, 2).to_s
+# ans = quadratic(1, 1, -6)
+# ans = quadratic(6, 7, -3)
+# ans = quadratic(1, -5, 4)
+# ans = quadratic(1, 8, 16)
+# ans = quadratic(2, 4, 1)
+# ans = quadratic(9, 6, 1)
+ans = quadratic(3, 3, 2)
+
+puts "x1: #{ans[0]}"
+puts "x2: #{ans[1].nil? ? ans[0] : ans[1]}"
